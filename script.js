573975a7811aa9adf5444dc6424eb5647a11c8de
davenevis <91218465+davenevis@users.noreply.github.com>
2023-05-25 23:25:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const terminalOutput = document.getElementById('terminal-output');
 const terminalCursor = document.getElementById('terminal-cursor');
-const commandTexts = ['run decryption davenevis.txt', '...processing...', 'decryption successful', 'opening davenevis.txt',];
+const commandTexts = ['run rootdecrypt.py', '...processing...', 'decryption successful', 'opening davenevis.txt',];
 
 const cursorBlinkDelay = 500; // Adjust the blinking delay as desired
 
