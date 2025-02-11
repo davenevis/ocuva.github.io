345fd5b44117f63d1c8ef399d8ed5d770a653d7d
davenevis <91218465+davenevis@users.noreply.github.com>
2025-02-11 09:35:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Scene setup
 const scene = new THREE.Scene();
